b534c45176cd502ef3c273894dd95e7233778a2a
Madhusudhan <Madhusudhan1863@gmail.com>
2026-04-28 09:04:25 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let maxProfit = 0;
+        let minBuy = prices[0];
+
+        for (let i = 0; i < prices.length; i++) {
+            maxProfit = Math.max(maxProfit, prices[i] - minBuy);
+            minBuy = Math.min(minBuy, prices[i]);
+        }
+
+        return maxProfit;
+    }
+}
